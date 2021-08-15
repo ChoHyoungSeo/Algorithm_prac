3ce433f7bfcd4c6bb836529565e466c3fbfd8f2f
ChoHyoungSeo <francesco.johs@gmail.com>
2021-08-16 02:57:30 +0900

    Update 10870.py

--- boj/10870.py
+++ boj/10870.py
@@ -1,3 +1,4 @@
+#recursion
 def fib(n: int) -> int:
     if n == 0:
         return 0
@@ -8,3 +9,19 @@ def fib(n: int) -> int:
 
 if __name__ == '__main__':
     print(fib(int(input())))
+    
+#================================
+#iteration
+# def fib02(n):
+#     if n == 0:
+#         print(0)
+#     elif n == 1:
+#         print(1)
+#     else:
+#         a = 0
+#         b = 1
+#         for i in range(2,n+1):
+#             c = a+b
+#             a = b
+#             b =c
+#     return c
