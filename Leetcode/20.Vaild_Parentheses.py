266665223fc242a13dcefea582fa2223319855e1
ChoHyoungSeo <francesco.johs@gmail.com>
2021-08-26 02:59:34 +0900

    Update 20.Vaild_Parentheses.py

--- Leetcode/20.Vaild_Parentheses.py
+++ Leetcode/20.Vaild_Parentheses.py
@@ -26,3 +26,31 @@ class Solution:
             return False
         else:
             return True
+        
+#Runtime: 32 ms, faster than 62.10% of Python3 online submissions for Valid Parentheses.
+#Memory Usage: 14 MB, less than 96.63% of Python3 online submissions for Valid Parentheses.
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         temp = []
+#         for i in range(len(s)):
+#             if s[i] == "(" or s[i] == "{" or s[i] == "[":
+#                 temp.append(s[i])
+#             elif s[i] == ")":
+#                 if temp and temp[-1] == "(":
+#                     del temp[-1]
+#                 else:
+#                     return False
+#             elif s[i] == "}":
+#                 if temp and temp[-1] == "{":
+#                     del temp[-1]
+#                 else:
+#                     return False
+#             elif s[i] == "]":
+#                 if temp and temp[-1] == "[":
+#                     del temp[-1]
+#                 else:
+#                     return False
+#         if temp:
+#             return False
+#         else:
+#             return True
