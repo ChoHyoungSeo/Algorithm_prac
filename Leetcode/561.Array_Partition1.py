32092c652b1dd0971f3b410c10e828138a5e5c7e
chohyoungseo <francesco.johs@gmail.com>
2021-09-12 01:44:35 +0900

    update leetcode Array_Partition1.py

--- Leetcode/561.Array_Partition1.py
+++ Leetcode/561.Array_Partition1.py
@@ -7,3 +7,39 @@ class Solution:
         for i in range(0,len(nums), 2):
             ans += nums[i]
         return ans
+        
+
+# # Runtime: 323 ms, faster than 18.02% of Python3 online submissions for Array Partition I.
+# # Memory Usage: 16.7 MB, less than 68.22% of Python3 online submissions for Array Partition I.
+# class Solution:
+#     def arrayPairSum(self, nums: List[int]) -> int:
+#         return sum(sorted(nums)[::2])
+
+
+# # Runtime: 410 ms, faster than 9.07% of Python3 online submissions for Array Partition I.
+# # Memory Usage: 16.9 MB, less than 31.92% of Python3 online submissions for Array Partition I.
+# class Solution:
+#     def arrayPairSum(self, nums: List[int]) -> int:
+#         sum = 0
+#         nums.sort()
+        
+#         for i, n in enumerate(nums):
+#             if i % 2 == 0:
+#                 sum += n
+#         return sum
+
+
+# # Runtime: 406 ms, faster than 9.53% of Python3 online submissions for Array Partition I.
+# # Memory Usage: 16.8 MB, less than 68.22% of Python3 online submissions for Array Partition I.
+# class Solution:
+#     def arrayPairSum(self, nums: List[int]) -> int:
+#         sum = 0
+#         pair = []
+#         nums.sort()
+        
+#         for n in nums:
+#             pair.append(n)
+#             if len(pair) == 2:
+#                 sum += min(pair)
+#                 pair = []
+#         return sum
