2548b7f482aed07c46b20fa85cd6c417ea9d1d07
chohyoungseo <francesco.johs@gmail.com>
2021-09-19 23:32:15 +0900

    init

---
+++ boj/9375.py
@@ -0,0 +1,15 @@
+# 결국 (의상 종류의 개수 + 1) * (의상 종류의 개수 + 1) * ... -1 
+# temp
+from collections import Counter
+t = int(input())
+for i in range(t):
+    n = int(input())
+    s = []
+    for j in range(n):
+        a, b = input().split()
+        s.append(b)
+    num = 1
+    result = Counter(s)
+    for key in result:
+        num *= result[key] + 1
+    print(num - 1)
