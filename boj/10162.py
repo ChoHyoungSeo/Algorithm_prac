5e235cc33de2261e9090cbc5b164bff074cdc215
chohyoungseo <francesco.johs@gmail.com>
2022-01-30 00:20:44 +0900

    add boj 10162.py

---
+++ boj/10162.py
@@ -0,0 +1,21 @@
+#10162
+tot = int(input())
+# 300 60 10
+
+# if tot % 10 != 0:
+#     print(-1)
+# else:
+#     a = tot // 300
+#     b = (tot-(a*300)) // 60
+#     c = (tot-(a*300) - (b*60)) // 10
+#     print(a, b, c)
+
+tot 변환해주는게 편할듯
+# # else:
+# #     a = tot // 300
+#     tot %= 300
+# #     b = (tot-(a*300)) // 60
+# tot %= 60
+# #     c = (tot-(a*300) - (b*60)) // 10
+# tot %= 10
+# #     print(a, b, c)
