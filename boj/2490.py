a41d15fdbd4bb844bb2bdb7389b4efa06e2fe3e9
chohyoungseo <francesco.johs@gmail.com>
2022-02-07 21:40:21 +0900

    add boj 2490.py

---
+++ boj/2490.py
@@ -0,0 +1,13 @@
+for _ in range(3):
+    num = sum(list(map(int, input().split())))
+    if num == 0:
+        print("D")
+    elif num == 1:
+        print("C")
+    elif num == 2:
+        print("B")
+    elif num == 3:
+        print("A")
+    # elif num == 4:
+    else:
+        print("E")
