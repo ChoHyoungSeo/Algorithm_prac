297c70bd2e1786d842fd8c4546edf0167bd84bcf
ChoHyoungSeo <francesco.johs@gmail.com>
2022-03-16 16:24:44 +0900

    Update 11659.py

--- boj/11659.py
+++ boj/11659.py
@@ -1,23 +1,14 @@
 #11659
-# timeout
-# import sys
-
-# tot_nums, tar = map(int, sys.stdin.readline().strip().split())
-# nums = list(map(int, sys.stdin.readline().strip().split()))
-
-# for _ in range(tar):
-#     start, end = map(int, sys.stdin.readline().strip().split())
-#     print(sum(nums[start-1:end]))
-
 #use prefix_sum
+
 import sys
  
 tot_nums, tar = map(int, sys.stdin.readline().strip().split())
 nums = list(map(int, sys.stdin.readline().strip().split()))
-prefix_sum = [0]    # init prefix_sum
+prefix_sum = [0]
 temp = 0
 
-for i in nums:    # accumulate arr section 
+for i in nums:
     temp += i
     prefix_sum.append(temp)
  
