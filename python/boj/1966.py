92e0f90726574158b5f0a721e241d9951ccc8480
ChoHyoungSeo <francesco.johs@gmail.com>
2023-06-29 15:36:02 +0900

    update boj 1966.py

--- python/boj/1966.py
+++ python/boj/1966.py
@@ -1,20 +1,40 @@
-a = int(input())
+# a = int(input())
 
-for _ in range(a):
-    tot, target = list(map(int, input().split()))
-    imp = list(map(int, input().split()))
-    idx = list(range(len(imp)))
-    idx[target] = 'target'
-    loc = 0
+# for _ in range(a):
+#     tot, target = list(map(int, input().split()))
+#     imp = list(map(int, input().split()))
+#     idx = list(range(len(imp)))
+#     idx[target] = 'target'
+#     loc = 0
+#     while True:
+#         if imp[0] == max(imp):
+#             loc += 1       
+#             if idx[0] == 'target':
+#                 print(loc)
+#                 break
+#             else:
+#                 imp.pop(0)
+#                 idx.pop(0)
+#         else:
+#             imp.append(imp.pop(0))
+#             idx.append(idx.pop(0))
+
+test_case = int(input())
+
+for _ in range(test_case):
+    n, m = list(map(int, input().split(' ')))
+    queue = list(map(int, input().split(' ')))
+    queue = [(i, idx) for idx, i in enumerate(queue)]
+    
+    count = 0
     while True:
-        if imp[0] == max(imp):
-            loc += 1       
-            if idx[0] == 'target':
-                print(loc)
+        if queue[0][0] == max(queue, key=lambda x: x[0])[0]:
+            count += 1
+            if queue[0][1] == m:
+                print(count)
                 break
             else:
-                imp.pop(0)
-                idx.pop(0)
+                queue.pop(0)
         else:
-            imp.append(imp.pop(0))
-            idx.append(idx.pop(0))
+            queue.append(queue.pop(0))
+            
