793194063371d4e05ab37c2ee7c1c09e23b3dc8e
ChoHyoungSeo <francesco.johs@gmail.com>
2023-07-03 17:54:22 +0900

    update boj 17298.py

--- python/boj/17298.py
+++ python/boj/17298.py
@@ -1,3 +1,24 @@
+# n = int(input())
+# arr = list(map(int, input().split()))
+# stack = []
+# NGE = [-1] * n
+
+# for i in range(n):
+#     x = arr[i]
+#     if len(stack) == 0 or stack[-1][0] >= x:
+#         stack.append((x,i)) #(수, idx) 내림차순 일 때 넣어주기
+#     else:
+#         while len(stack) > 0: #역방향 꺼내기
+#             previous, idx = stack.pop()
+#             if previous >= x:
+#                 stack.append((previous, idx))
+#                 break
+#             else:
+#                 NGE[idx] = x #오큰수 기록
+#         stack.append((x,i))
+# for x in NGE:
+#     print(x, end = ' ')
+
 import sys
 
 _ = int(sys.stdin.readline().strip())
