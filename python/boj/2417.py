6645f5909a15bba62993e476571374bc3aa43894
ChoHyoungSeo <francesco.johs@gmail.com>
2023-07-04 02:08:11 +0900

    Update boj 2417.py

--- python/boj/2417.py
+++ python/boj/2417.py
@@ -1,3 +1,17 @@
-import math
-a = int(input())
-print(math.ceil(a**(1/2)))
+# import math
+# a = int(input())
+# print(math.ceil(a**(1/2)))
+
+n = int(input())
+ 
+start = 0
+end = n
+ 
+while start <= end:
+    mid = (start + end) // 2
+    if mid ** 2 < n:
+        start = mid + 1
+    else:
+        end = mid - 1
+ 
+print(start)
