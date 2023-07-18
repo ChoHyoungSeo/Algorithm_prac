66ca558f1674d3394ee52b75e5e9a35c9ee83f44
ChoHyoungSeo <francesco.johs@gmail.com>
2023-07-18 15:42:48 +0900

    update boj 2014.py

---
+++ python/boj/2014.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+import heapq
+
+k, n = map(int, input().split())
+data = list(map(int, input().split()))
+heap = [] #최소 힙
+visited = set()
+max_value = max(data)
+
+for x in data:
+    heapq.heappush(heap, x)
+
+for i in range(n-1):
+    element = heapq.heappop(heap)
+    for x in data:
+        now = element * x
+        if len(heap) >= n and max_value < now:
+            continue
+        if now not in visited:
+            heapq.heappush(heap, now)
+            max_value = max(max_value, now)
+            visited.add(now)
+print(heapq.heappop(heap))
