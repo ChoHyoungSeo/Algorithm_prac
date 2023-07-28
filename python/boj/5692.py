404b8b6c776d6398958005fdb6e5d92ee5bf1027
ChoHyoungSeo <francesco.johs@gmail.com>
2023-07-28 18:01:19 +0900

    update boj 5692.py

--- python/boj/5692.py
+++ python/boj/5692.py
@@ -14,3 +14,19 @@ while True:
     for i in range(len(num)):
         ans += int(num[len(num)-i-1]) * factorial(i+1)
     print(ans)
+
+
+# lambda식 사용
+import sys
+
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n-1)
+
+while True:
+    num = sys.stdin.readline().strip()
+    if int(num) == 0:
+        break    
+    n = len(num)
+    print(sum(map(lambda x : int(num[x])*factorial(n-x), range(n))))
