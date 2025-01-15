07ca935132ee37857732792c91068e6c72226165
ChoHyoungSeo <francesco.johs@gmail.com>
2025-01-15 14:52:07 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 96 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,24 +1,20 @@
-import sys
-input = sys.stdin.readline
+#circular/doubly linked list 스타일로 구현해봐도 재미있을 듯.
 from collections import deque
 
-n = int(input())
-arr = list(map(int, input().split()))
-d=deque()
-for i in range(n):
-    d.append((arr[i], i+1))
-result = []
+_ = int(input())
+nums = list(map(int, input().split()))
+queue = deque((idx+1, num) for idx, num in enumerate(nums))
+idx, cur = queue.popleft()
+ans = [idx] #index 번호
 
-current, index = d.popleft()
-result.append(index)
-for i in range(n-1):
-    if current > 0:
-        for j in range(current-1):
-            d.append(d.popleft())
+for _ in range(len(nums)-1):
+    if cur > 0:
+        for _ in range(cur-1):
+            queue.append(queue.popleft())
     else:
-        for j in range(-current):
-            d.appendleft(d.pop())
-    current, index = d.popleft()
-    result.append(index)
-for x in result:
-    print(x, end=' ')
+        for _ in range(-cur):
+            queue.appendleft(queue.pop())
+    idx, cur = queue.popleft()
+    ans.append(idx)
+
+print(*ans)
