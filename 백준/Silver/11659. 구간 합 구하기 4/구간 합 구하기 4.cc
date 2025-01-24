b310c81bb1c04e461c588d9e1fbd93860b794a05
ChoHyoungSeo <francesco.johs@gmail.com>
2025-01-24 16:22:29 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int n, m;
+  cin >> n >> m;
+  int sum[100001];
+
+  for (int i = 1; i <= n; i++){
+    int temp;
+    cin >> temp;
+    sum[i] = sum[i-1] + temp;
+  }
+
+  for (int i = 0; i < m; i++){
+    int start, end;
+    cin >> start >> end;
+    cout << sum[end] - sum[start-1] << "\n";
+  }
+}
