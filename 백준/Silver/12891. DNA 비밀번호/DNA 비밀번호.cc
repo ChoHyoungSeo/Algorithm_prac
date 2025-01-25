ee6da7e4b9d2429097ae788f3d6f32d5b3bd5c25
ChoHyoungSeo <francesco.johs@gmail.com>
2025-01-26 01:21:51 +0900

    [Silver II] Title: DNA 비밀번호, Time: 16 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.cc
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.cc
@@ -1,67 +1,92 @@
 #include <iostream>
-#include <string>
-#include <vector>
 using namespace std;
 
+int checkArr[4];
+int myArr[4];
+int checkSecret = 0;
+void Add(char c);
+void Remove(char c);
+
 int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
   int s, p;
-  string DNA;
-  vector<int> required(4);
-  vector<int> current(4,0);
-  int validCnt = 0;
-  
   cin >> s >> p;
-  cin >> DNA;
+  int result = 0;
+  string A;
+  cin >> A;
 
   for (int i = 0; i < 4; i++){
-    cin >> required[i];
+    cin >> checkArr[i];
+    if (checkArr[i] == 0){
+      checkSecret++;
+    }
   }
-
   for (int i = 0; i < p; i++){
-    switch (DNA[i]){
-      case 'A': current[0]++; break;
-      case 'C': current[1]++; break;
-      case 'G': current[2]++; break;
-      case 'T': current[3]++; break;
-
-    }
+    Add(A[i]);
   }
-
-  bool isValid = true;
-  for (int i = 0; i < 4; i++){
-    if (current[i] < required[i]){
-      isValid = false;
-      break;
-    }
+  if (checkSecret == 4){
+    result++;
   }
-  if (isValid) validCnt++;
-
   for (int i = p; i < s; i++){
-    switch (DNA[i - p]){
-      case 'A': current[0]--; break;
-      case 'C': current[1]--; break;
-      case 'G': current[2]--; break;
-      case 'T': current[3]--; break;
+    int j = i - p;
+    Add(A[i]);
+    Remove(A[j]);
+    if (checkSecret == 4){
+      result++;
     }
-
-    switch (DNA[i]){
-      case 'A': current[0]++; break;
-      case 'C': current[1]++; break;
-      case 'G': current[2]++; break;
-      case 'T': current[3]++; break;
-    }
-
-    isValid = true;
-    for (int j = 0; j < 4; j++){
-      if (current[j] < required[j]){
-        isValid = false;
-        break;
-      }
-    }
-    if (isValid) validCnt++;
   }
+  cout << result << "\n";
+}
 
-  cout << validCnt << "\n";
+void Add(char c){
+  switch (c){
+    case 'A':
+    myArr[0]++;
+    if (myArr[0] == checkArr[0])
+      checkSecret++;
+    break;
 
-  return 0;
+    case 'C':
+    myArr[1]++;
+    if (myArr[1] == checkArr[1])
+      checkSecret++;
+    break;
+    case 'G':
+    myArr[2]++;
+    if (myArr[2] == checkArr[2])
+      checkSecret++;
+    break;
+    case 'T':
+    myArr[3]++;
+    if (myArr[3] == checkArr[3])
+      checkSecret++;
+    break;    
+  }
+}
+
+void Remove(char c){
+  switch (c){
+    case 'A':
+    if (myArr[0] == checkArr[0])
+      checkSecret--;
+    myArr[0]--;
+    break;
+    case 'C':
+    if (myArr[1] == checkArr[1])
+      checkSecret--;
+    myArr[1]--;
+    break;
+    case 'G':
+    if (myArr[2] == checkArr[2])
+      checkSecret--;
+    myArr[2]--;
+    break;
+    case 'T':
+    if (myArr[3] == checkArr[3])
+      checkSecret--;
+    myArr[3]--;
+    break;
+  }
 }
