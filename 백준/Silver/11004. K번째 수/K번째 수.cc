a4fde1ef12654f959dcb4504d8a80e1e6b3bf9f3
ChoHyoungSeo <francesco.johs@gmail.com>
2025-01-28 00:51:06 +0900

    [Silver V] Title: K번째 수, Time: 608 ms, Memory: 21552 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.cc
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -1,23 +1,93 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-int main() {
+void quickSort(vector<int> &A, int S, int E, int K);
+int partition(vector<int> &A, int S, int E);
+void swap(vector<int> &A, int i, int j);
+
+int main(){
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(nullptr); cout.tie(nullptr);
 
     int n, k;
     cin >> n >> k;
-    vector<int> arr(n);
+    vector<int> A(n);
 
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
+    for (int i = 0; i<n; ++i){
+        cin >> A[i];
     }
-
-    sort(arr.begin(), arr.end());
-
-    cout << arr[k - 1] << "\n";
-
-    return 0;
+    quickSort(A, 0, n-1, k-1);
+    cout << A[k-1];
 }
+
+void quickSort(vector<int> &A, int S, int E, int K){
+    int pivot = partition(A, S, E);
+    if (pivot == K){
+        return;
+    }
+    else if (K < pivot){
+        quickSort(A, S, pivot-1, K);
+    }
+    else{
+        quickSort(A, pivot+1, E, K);
+    }
+}
+
+int partition(vector<int> &A, int S, int E){
+    if (S+1 == E){
+        if (A[S] > A[E]){
+            swap(A, S, E);
+        }
+        return E;
+    }
+    int M = (S+E) / 2;
+    swap(A, S, M);
+    int pivot = A[S];
+    int i = S+1, j = E;
+    while (i<=j){
+        while (pivot < A[j] && j>0){
+            j--;
+        }
+        while (pivot > A[i] && i < A.size()-1){
+            i++;
+        }
+        if (i<=j){
+            swap(A, i++, j--);
+        }
+    }
+    A[S] = A[j];
+    A[j] = pivot;
+    return j;
+}
+
+void swap(vector<int> &A, int i, int j){
+    int temp = A[i];
+    A[i] = A[j];
+    A[j] = temp;
+}
+
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     int n, k;
+//     cin >> n >> k;
+//     vector<int> arr(n);
+
+//     // 배열 입력
+//     for (int i = 0; i < n; ++i) {
+//         cin >> arr[i];
+//     }
+
+//     sort(arr.begin(), arr.end());
+
+//     cout << arr[k - 1] << "\n";
+
+//     return 0;
+// }
