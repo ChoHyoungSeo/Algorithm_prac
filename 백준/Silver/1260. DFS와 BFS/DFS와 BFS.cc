b8f0bbfd60e608328ff3d7b97e7075ac2c1034c6
ChoHyoungSeo <francesco.johs@gmail.com>
2025-01-29 01:51:53 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+void dfs(int node, vector<vector<int>>& graph, vector<bool>& visited);
+void bfs(int start, vector<vector<int>>& graph, vector<bool>& visited);
+
+int main(){
+    int n, m, start;
+    cin >> n >> m >> start;
+
+    vector<vector<int>> graph(n+1);
+    vector<bool> visited(n+1, false);
+
+    for (int i = 0; i<m; ++i){
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    for (int i = 1; i<=n; ++i){
+        sort(graph[i].begin(), graph[i].end());
+    }
+
+    dfs(start, graph, visited);
+    cout << "\n";
+
+    fill(visited.begin(), visited.end(), false);
+    bfs(start, graph, visited);
+
+    return 0;
+}
+
+void dfs(int node, vector<vector<int>>& graph, vector<bool>& visited){
+    visited[node]= true;
+    cout << node << " ";
+
+    for (int next: graph[node]){
+        if (!visited[next]){
+            dfs(next, graph, visited);
+        }
+    }
+}
+
+void bfs(int start, vector<vector<int>>& graph, vector<bool>& visited){
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
+
+    while (!q.empty()){
+        int node = q.front();
+        q.pop();
+        cout << node << " ";
+
+        for (int next: graph[node]){
+            if (!visited[next]){
+                visited[next] = true;
+                q.push(next);
+            }
+        }
+    }
+}
