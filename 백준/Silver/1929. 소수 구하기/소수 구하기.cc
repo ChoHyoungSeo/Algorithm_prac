ed95460ea5b4756aa72505c9e1ba8c6a7f40c56e
ChoHyoungSeo <francesco.johs@gmail.com>
2025-02-02 22:51:17 +0900

    [Silver III] Title: 소수 구하기, Time: 8 ms, Memory: 2260 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,32 +1,68 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 using namespace std;
 
-int main(){
+void sieve(int m, int n) {
+    vector<bool> is_prime(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= n; j += i) { 
+                is_prime[j] = false;
+            }
+        }
+    }
+
+    for (int i = m; i <= n; i++) {
+        if (is_prime[i]) {
+            cout << i << '\n';
+        }
+    }
+}
+
+int main() {
     ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
 
     int m, n;
     cin >> m >> n;
-    vector<int> prime(n+1);
 
-    for (int i = 2; i<=n; i++){
-        prime[i] = i;
-    }
+    sieve(m, n);
 
-    for (int i = 2; i<=sqrt(n); i++){
-        if (prime[i] == 0){
-            continue;
-        }
-        for (int j = i+i; j<=n; j=j+i){
-            prime[j] = 0;
-        }
-    }
-    for (int i = m; i<=n; i++){
-        if (prime[i] != 0){
-            cout << prime[i] << "\n";
-        }
-    }
     return 0;
 }
+
+
+// #include <iostream>
+// #include <vector>
+// #include <cmath>
+// using namespace std;
+
+// int main(){
+//     ios::sync_with_stdio(false);
+//     cin.tie(0); cout.tie(0);
+
+//     int m, n;
+//     cin >> m >> n;
+//     vector<int> prime(n+1);
+
+//     for (int i = 2; i<=n; i++){
+//         prime[i] = i;
+//     }
+
+//     for (int i = 2; i<=sqrt(n); i++){
+//         if (prime[i] == 0){
+//             continue;
+//         }
+//         for (int j = i+i; j<=n; j=j+i){
+//             prime[j] = 0;
+//         }
+//     }
+//     for (int i = m; i<=n; i++){
+//         if (prime[i] != 0){
+//             cout << prime[i] << "\n";
+//         }
+//     }
+//     return 0;
+// }
