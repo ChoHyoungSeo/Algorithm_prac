584d353932f5868538f54055699ef46093b892bc
ChoHyoungSeo <francesco.johs@gmail.com>
2025-02-18 13:32:40 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 404 ms, Memory: 21724 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -1,62 +1,63 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
+#include <algorithm>
 using namespace std;
 
-int n, m, k, x;
+void bfs(int node);
 vector<vector<int>> graph;
-vector<int> dist;
+vector<int> visited;
 vector<int> ans;
 
-void bfs(int start){
-  queue<int> q;
-  q.push(start);
-  dist[start] = 0;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
 
-  while (!q.empty()){
-    int current = q.front();
-    q.pop();
+    int n, m, k, x;
+    cin >> n >> m >> k >> x;
 
-    for (int next : graph[current]){
-      if (dist[next] == -1){
-        dist[next] = dist[current] + 1;
-        q.push(next);
-      }
+    visited.resize(n+1,0);
+    graph.resize(n+1);
+    
+    for(int i=0; i<m; ++i){
+        int a, b;
+        cin >> a >> b;
+        graph[a].push_back(b);
     }
-  }
+    
+    bfs(x);
+
+    for(int i=0; i<visited.size(); ++i){
+        if (visited[i] == k+1){
+            ans.push_back(i);
+        }
+    }
+    if (!ans.empty()){
+        sort(ans.begin(), ans.end());
+        for(int i=0; i < ans.size(); ++i){
+            cout << ans[i] << "\n";
+        }
+    } else{
+        cout << "-1" << "\n";
+    }
+
+    return 0;
 }
 
-int main(){
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr); cout.tie(nullptr);
+void bfs(int node){
+    queue<int> q;
+    q.push(node);
+    visited[node]++;
 
-  cin >> n >> m >> k >> x;
-  graph.resize(n+1);
-  dist.assign(n+1, -1);
-
-  for (int i = 0; i<m; i++){
-    int a, b;
-    cin >> a >> b;
-    graph[a].push_back(b);
-  }
-
-  bfs(x);
-
-  for (int i = 1; i<=n; i++){
-    if (dist[i] == k){
-      ans.push_back(i);
+    while(!q.empty()){
+        int now = q.front();
+        q.pop();
+        for (int i : graph[now]){
+            if (visited[i] == 0){
+                visited[i] = visited[now]+1;
+                q.push(i);
+            }
+        }
+        
     }
-  }
-
-  if (ans.empty()){
-    cout << "-1";
-  } else {
-    sort(ans.begin(), ans.end());
-    for (int city : ans){
-      cout << city << "\n";
-    }
-  }
-
-  return 0;
 }
